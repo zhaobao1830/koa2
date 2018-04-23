cfdab7b8a0cb5e4ffb96bbfca680f670c7f208a2
赵宝 <zhaobao@lubanec.com>
2018-04-23 17:11:23 +0800

    1

--- demo04.js
+++ demo04.js
@@ -1,5 +1,8 @@
 const Koa = require('koa')
+const bodyParser = require('koa-bodyparser');
 const app = new Koa()
+
+app.use(bodyParser())
 app.use(async(ctx) => {
   //当请求时GET请求时，显示表单让用户填写
   if (ctx.url==='/' && ctx.method === 'GET') {
@@ -18,40 +21,13 @@ app.use(async(ctx) => {
     ctx.body = html;
     // post请求
   } else if (ctx.url=== '/' && ctx.method=== 'POST') {
-    let postData = await parsePostData(ctx)
+    let postData = ctx.request.body
     ctx.body = postData
   }else{
     // 其他请求
     ctx.body = '<h1>404!</h1>'
   }
 })
-function parsePostData (ctx) {
-  return new Promise((resolve, reject) => {
-    try {
-      let postData = ''
-      ctx.req.on('data',(data) => {
-        postData += data
-      })
-      ctx.req.on('end',function () {
-        let parseData = parseQueryStr(postData)
-        resolve(parseData)
-      })
-    }catch(error) {
-       reject(error)
-    }
-  })
-}
-function parseQueryStr(queryStr){
-  let queryData={};
-  console.log(queryStr)
-  let queryStrList = queryStr.split('&');
-  for( let [index,queryStr] of queryStrList.entries() ){
-    let itemList = queryStr.split('=');
-    console.log(itemList);
-    queryData[itemList[0]] = decodeURIComponent(itemList[1]);
-  }
-  return queryData
-}
 app.listen(3000,()=>{
   console.log('[demo] server is starting at port 3000');
 })
